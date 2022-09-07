337ef1846bee624f812828159bbd5de160e27668
Mykyta Yefruiev <myf@unicsoft.com>
2022-09-07 13:30:22 +0200

    Fixed 404 page

--- src/components/Page404Component/Page404Component.tsx
+++ src/components/Page404Component/Page404Component.tsx
@@ -3,14 +3,17 @@ import Container from '@components/shared/Container';
 import NavButton from '@components/shared/NavButton';
 
 import styles from './Page404.module.scss';
+import NotFoundSVG from "@components/Page404Component/NotFoundSVG";
 
 const Page404Component = () => {
   return (
     <Container className={styles.container}>
       <div className={styles.inner}>
-        <h1>404</h1>
+        <NotFoundSVG />
+
         <div className={styles.header}>We are sorry...</div>
-        <p>The page you are looking for could not be found.</p>
+        <p>The page is not found.</p>
+
         <NavButton
           className={styles.button}
           to='/'
@@ -27,3 +30,6 @@ const Page404Component = () => {
 };
 
 export default Page404Component;
+
+
+
