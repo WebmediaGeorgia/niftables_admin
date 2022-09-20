c09244163319e0b7606c3698010661083b970984
Mykyta Yefruiev <myf@unicsoft.com>
2022-09-20 17:30:47 +0200

    Fixed sold out in packs.

--- src/componentsV2/global-modal/initial-modals/packs-details/detail-info/SoldOut.tsx
+++ src/componentsV2/global-modal/initial-modals/packs-details/detail-info/SoldOut.tsx
@@ -1,17 +1,43 @@
 import React from 'react'
 import get from 'lodash/get'
 
-import LegendBlock from '@commonV2/LegendBlock'
+import styled from "styled-components";
 
 export default function SoldOut ({ pack }) {
   const availableSupply = get(pack, 'availableSupply')
   const packsNft = get(pack, 'packsNft')
   if (availableSupply === 0 && !packsNft) {
     return (
-      <LegendBlock label='Sold out'>
-        <div>Lorem ipsum dolor sit amet, consectetur.</div>
-      </LegendBlock>
+      <SoldOutContainer>
+        <div className="title">Sold out</div>
+        <div className="description">Lorem ipsum dolor sit amet, consectetur.</div>
+      </SoldOutContainer>
     )
   }
   return null;
 }
+
+const SoldOutContainer = styled.div`
+  background: rgba(7, 3, 31, 0.3);
+  border-radius: 100px;
+  margin-top: 15px;
+  padding: 7px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .title {
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 22px;
+    color: #FF66C4;
+    text-transform: uppercase;
+  }
+
+  .description {
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 18px;
+    color: white;
+  }
+`;
