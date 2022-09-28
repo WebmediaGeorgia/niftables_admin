a2de92c9ae1a47a74823190d365704e8f216d255
Mykyta Yefruiev <myf@unicsoft.com>
2022-09-28 14:20:22 +0200

    Fixed logo in maintenance mode

--- src/components/MaintenanceMode/MaintenanceMode.tsx
+++ src/components/MaintenanceMode/MaintenanceMode.tsx
@@ -8,19 +8,28 @@ import PageHeader from '@components/shared/PageHeader';
 import SocialIconsContainer from '@components/SocialIconsContainer';
 import GearsSVG from "@components/MaintenanceMode/GearsSVG";
 import Footer from "@components/footer";
+import HeaderLogo from "@components/header/header.logo";
+import Link from "next/link";
 
 export default function MaintenanceMode() {
   const router = useRouter();
 
-  // React.useEffect(() => {
-  //   $api
-  //     .get('/health/maintenance')
-  //     .then(() => router.push('/'))
-  //     .catch((error) => console.log(error));
-  // }, []);
+  React.useEffect(() => {
+    $api
+      .get('/health/maintenance')
+      .then(() => router.push('/'))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div className={styles.wrapper}>
+      <div className={styles.logo}>
+        <HeaderLogo />
+        <Link href='/'>
+          <a>Superlotl</a>
+        </Link>
+      </div>
+
       <Container className={styles.container}>
         <div className={styles.media}><GearsSVG /></div>
         <div className={styles.content}>
